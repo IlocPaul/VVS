b3635a89c665eaded1b4c4103fc1b84f0754ee80
IlocPaul <ilocpaul@yahoo.com>
2020-12-17 22:25:02 +0200

    selenium updated

--- web-gui-selenium/ConsoleApp1/ConsoleApp1/Program.cs
+++ web-gui-selenium/ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp1
            
 
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://localhost:44354/";
+            driver.Url = "https://localhost:44318/";
 
             //fullscreen + asteptam 10 secunde
             driver.Manage().Window.Maximize();
@@ -30,7 +30,32 @@ namespace ConsoleApp1
             Task.Delay(3000).Wait();
 
              driver.FindElement(By.XPath("//a[@href='/' and text()='Home']")).Click();
-           // driver.FindElement(By.XPath("//a[@href='/Home/Contact' and text()='Contact']")).Click();
+            // driver.FindElement(By.XPath("//a[@href='/Home/Contact' and text()='Contact']")).Click();
+
+            Task.Delay(1000).Wait();
+            driver.FindElement(By.Id("registerLink")).Click();
+
+            //ne folosim the input box si incercam sa ne inregistram (am facut sa dea fail pt a putea repeta testul de mai multe ori)
+            driver.FindElement(By.Id("Email")).SendKeys("userNou@yahoo.com");
+            Task.Delay(1000).Wait();
+            driver.FindElement(By.Id("Password")).SendKeys("parola123");
+            Task.Delay(1000).Wait();
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("parola123");
+            Task.Delay(1000).Wait();
+
+            //apasam pe buton pt a ne inregistra
+            driver.FindElement(By.ClassName("btn-default")).Click();
+
+            Task.Delay(2000).Wait();
+            driver.FindElement(By.XPath("//a[@href='/' and text()='Home']")).Click();
+            //ne intoarcem la pagina principala si validam un text
+
+            var data = driver.FindElement(By.ClassName("lead")).Text;
+            Console.WriteLine("Textul este: {0} ",data);
+            //dam click pe un link
+            driver.FindElement(By.XPath("//a[@href='https://go.microsoft.com/fwlink/?LinkId=301865' and text()='Learn more »']")).Click();
+          
+            
         }
     }
 }
---
+++ web-gui-selenium/readme.txt
@@ -0,0 +1,9 @@
+-am folosit 2 aplicatii pentru testarea cu selenium
+
+-in folder GUI se afla web server-ul si interfata grafica facute in C# ASP.Net MVC
+
+-in folder ConsoleApp1 se afla testele cu selenium
+
+-prima data se da start la aplicatia din GUI apoi se pot rula testele din ConsoleApp1
+
+-am adaugat si un video, demo selenium, in care se face testarea unde se poate observa comportamentul testelor
